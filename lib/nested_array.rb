5a01056d16d7de51e3e5f10b08b11d6ac289ede5
Kelly Becker <kelly.lynn.becker@gmail.com>
2019-12-18 00:08:30 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -25,8 +25,7 @@ def sorted_matrix
 end
 
 def matrix_lookup(matrix, row, column)
-  matrix = [CONVENTIONAL_PRODUCE, ORGANIC_PRODUCE]
-  matrix.slice(row,column)
+  matrix[row][column]
 end
 
 def matrix_update(matrix, row, column, new_value)
